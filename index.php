1262c750787293851336d32aa579a18b7404f57a
AlexandrDulovec <79139997+AlexandrDulovec@users.noreply.github.com>
2021-09-24 13:49:52 +0200

    index.php

--- index.php
+++ index.php
@@ -157,40 +157,49 @@ $all =$text . $amount . $convertfrom . " = " . $final . $convertto ?>
 <form action="index.php" method="post">
 <br>
 <h1>Převod</h1> <br> 
-<label for="castka">Částka:</label> 
-<input type="number" id="castka" name="castka"> <br> 
-<br>
-    Koruny na Eura: <input type="radio" name="switch" value="czk_eur" id="switch"><br>
-    Eura na Koruny: <input type="radio" name="switch" value="eur_czk" id="switch"><br>
-        <br>
-    Koruny na Dolary: <input type="radio" name="switch" value="czk_usd" id="switch"><br>
-    Dolary na Koruny: <input type="radio" name="switch" value="usd_czk" id="switch"><br>
-        <br>
-    Koruny na Rubly: <input type="radio" name="switch" value="czk_rub" id="switch"><br>
-    Rubly na Koruny: <input type="radio" name="switch" value="rub_czk" id="switch"><br>
-       <br>
-    Koruny na Libry: <input type="radio" name="switch" value="czk_gbp" id="switch"><br>
-    Libry na Koruny: <input type="radio" name="switch" value="gbp_czk" id="switch"><br>     
-        <br>
-    Libry na Eura: <input type="radio" name="switch" value="gbp_eur" id="switch"><br>
-    Eura na Libry: <input type="radio" name="switch" value="eur_gbp" id="switch"><br>
-        
-    <br>        
-    Libry na Rubly <input type="radio" name="switch" value="gbp_rub" id="switch"><br>
-    Rubly na Libry: <input type="radio" name="switch" value="rub_gbp" id="switch"><br>
-       
-    <br>
-    Dolary na Eura: <input type="radio" name="switch" value="usd_eur" id="switch"><br>
-    Eura na Dolary: <input type="radio" name="switch" value="eur_usd" id="switch"><br>
-   <br>
-    Libry na Dolary: <input type="radio" name="switch" value="gbp_usd" id="switch"><br>
-    Dolary na Libry: <input type="radio" name="switch" value="usd_gbp" id="switch"><br>
-   <br>
-    Dolary na Rubly: <input type="radio" name="switch" value="usd_rub" id="switch"><br>
-    Rubly na Dolary: <input type="radio" name="switch" value="rub_usd" id="switch"><br>
-   <br>
-    Rubly na Eura: <input type="radio" name="switch" value="eur_rub" id="switch"><br>
-    Eura na Rubly: <input type="radio" name="switch" value="rub_eur" id="switch"><br>
+<label for="castka">Choose a car:</label>
+<select name="castka" id="castka">
+  <option value="czk_eur"> Koruny na Eura</option>
+  <option value="eur_czk">Eura na Koruny</option>
+  <option value="czk_usd">Koruny na Dolary</option>
+  <option value="usd_czk">Dolary na Koruny</option>
+  <option value="czk_rub">Koruny na Rubůy</option>
+  <option value="rub_czk">Rubly na Koruny</option>
+  <option value="czk_gbp">Koruny na Libry</option>
+  <option value="gbp_czk">Libry na Koruny</option>
+  <option value="gbp_eur">Libry na Eura</option>
+  <option value="eur_gbp">Eura na Libry</option>
+  <option value="gbp_rub">Libry na Rubly</option>
+  <option value="rub_gbp">Rubly na Libry</option>
+  <option value="usd_eur">Dolary na Eura</option>
+  <option value="eur_usd">Eura na Dolary:</option>
+  <option value="gbp_usd">Libry na Dolary</option>
+  <option value="usd_gbp">Dolary na Libry</option>
+  <option value="usd_rub">Dolary na Rubly</option>
+  <option value="rub_usd">Dolary na Libry</option>
+  <option value="eur_rub">Rubly na Eura</option>
+  <option value="rub_eur">Eura na Rubly</option>
+  <option value="eur_czk">Koruny na Eura</option>
+  <option value="czk_eur">Eura na Koruny</option>
+  <option value="czk_usd">Koruny na Dolary</option>
+  <option value="usd_czk">Dolary na Koruny</option>
+  <option value="czk_rub">Koruny na Rubly</option>
+  <option value="rub_czk">Rubly na Koruny</option>
+  <option value="czk_gnp">Koruny na Libry</option>
+  <option value="gbp_czk">Libry na Koruny</option>
+  <option value="gbp_eur">Libry na Eura</option>
+  <option value="eur_gbp">Eura na Libry</option>
+  <option value="gbp_rub">Libry na Rubly</option>
+  <option value="rub_gbp">Rubly na Libry</option>
+  <option value="usd_eur">Dolary na Eura</option>
+  <option value="eur_usd">Eura na Dolary</option>
+  <option value="gbp_usd">Libry na Dolary</option>
+  <option value="usd_gbp">Dolary na Libry</option>
+  <option value="usd_rub">Dolary na Rubly</option>
+  <option value="rub_usd">Rubly na Dolary</option>
+  <option value="eur_rub">Rubly na Eura</option>
+  <option value="rub_eur>Eura na Rubly</option>
+</select>
 <br>
 <input type= "submit" value="Odeslat" name="submit">
 </form>
